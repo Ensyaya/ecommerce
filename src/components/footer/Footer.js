16b2ced5e662ad20e3d1ed445857b2769823104f
Ensyaya <enesyayan92@gmail.com>
2022-07-14 15:21:24 +0300

    2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <footer className="p-4 bg-gray-900 shadow md:px-6 md:py-8 dark:bg-gray-900">
       <div className="sm:flex sm:items-center sm:justify-between">
         <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/2560px-Ford_logo_flat.svg.png"
             className="mr-3 h-8"
-            alt="Flowbite Logo"
+            alt="eCommerce logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             eCommerce
@@ -37,10 +37,10 @@ function Footer() {
           </li>
         </ul>
       </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <hr className="my-6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
-        <a href="https://flowbite.com/" className="hover:underline">
+        <a href="/#" className="hover:underline">
           eCommerce™
         </a>
         . All Rights Reserved.
